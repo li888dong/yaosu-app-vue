36433906ff91863393cee82e71f92f6a8f671754
李栋 <498437323@qq.com>
2018-03-27 17:01:15 +0800

    update

--- src/components/Person/Order/Order.vue
+++ src/components/Person/Order/Order.vue
@@ -1,40 +1,46 @@
 <style scoped>
-    .top-bar{
+    .top-bar {
         border-bottom: 5px solid #eeeeee;
     }
+
     .noaddress-container,
-    .address-container
-    {
+    .address-container {
         padding: 10px;
         height: 50px;
         line-height: 50px;
         text-align: center;
         font-size: 14px;
     }
-    .address-container>span{
+
+    .address-container > span {
         position: absolute;
     }
-    .address-container>.icon-dingwei{
+
+    .address-container > .icon-dingwei {
         position: absolute;
         left: 10px;
         top: 10px;
     }
-    .address-container>.shouhr{
+
+    .address-container > .shouhr {
         color: #000;
         left: 60px;
         top: -10px;
     }
-    .address-container>.phone{
+
+    .address-container > .phone {
         color: #000;
         left: 160px;
         top: -10px;
     }
-    .address-container>.dizhi{
+
+    .address-container > .dizhi {
         left: 60px;
         top: 10px;
         font-size: 12px;
     }
-    .address-container>.isMor{
+
+    .address-container > .isMor {
         right: 40px;
         top: 5px;
         font-size: 12px;
@@ -45,25 +51,38 @@
         line-height: 20px;
         color: #03A657;
     }
-    .address-container>.more{
+
+    .address-container > .more {
         position: absolute;
         right: 10px;
     }
-    .address-container>.type{
+
+    .address-container > .type {
         color: #03A657;
         right: 60px;
         bottom: -10px;
     }
-    .cartList{
+
+    .border {
+        position: relative;
+        top: 2px;
+        border-bottom: none;
+        border-top: 5px solid transparent;
+        background: linear-gradient(white, white) padding-box, repeating-linear-gradient(-45deg, red 0, red 12.5%, transparent 0, transparent 25%, #58a 0, #58a 37.5%, transparent 0, transparent 50%) 0 / 5em 5em;
+    }
+
+    .cartList {
         margin-top: 5px;
         position: relative;
     }
-    .goods-company{
+
+    .goods-company {
         height: 24px;
         padding: 10px 0 10px 20px;
         background-color: #fff;
     }
-    .goods-info{
+
+    .goods-info {
         height: 160px;
         padding: 5px 10px;
         margin-top: 1px;
@@ -73,36 +92,43 @@
         justify-content: flex-start;
 
     }
-    .goods-img{
+
+    .goods-img {
         width: 100px;
         height: 100px;
         margin: 10px;
         background-size: 100% 120px;
-        background-repeat:no-repeat;
+        background-repeat: no-repeat;
     }
-    .goods-desription{
+
+    .goods-desription {
         font-size: 14px;
         margin-top: 20px;
     }
-    .goods-name{
+
+    .goods-name {
         color: #000000;
     }
-    .goods-specification{
+
+    .goods-specification {
         margin-top: 20px;
         font-size: 14px;
     }
-    .goods-price{
+
+    .goods-price {
         margin-top: 10px;
         color: #f97433;
     }
-    .goods-amount{
+
+    .goods-amount {
         position: absolute;
         right: 20px;
         bottom: 60px;
         color: #000000;
         font-size: 16px;
     }
-    .goods-price-container{
+
+    .goods-price-container {
         position: absolute;
         border-top: 1px solid #eeeeee;
         bottom: 0;
@@ -114,7 +140,8 @@
         padding-right: 20px;
         font-size: 14px;
     }
-    .footer{
+
+    .footer {
         height: 30px;
         line-height: 30px;
         padding: 5px 10px;
@@ -127,7 +154,8 @@
         background-color: #fff;
         font-size: 14px;
     }
-    .footer .confirm-btn{
+
+    .footer .confirm-btn {
         position: absolute;
         right: 0;
         bottom: 0;
@@ -138,11 +166,13 @@
         text-align: center;
         line-height: 40px;
     }
-    .total-price{
+
+    .total-price {
         position: absolute;
         right: 150px;
     }
-    .fc-red{
+
+    .fc-red {
         color: #f97433;
     }
 </style>
@@ -158,12 +188,13 @@
             <span class="shouhr">{{receiveAddress.shouhr}}</span>
             <span class="phone">{{receiveAddress.phone}}</span>
             <span class="dizhi">{{receiveAddress.dizhi}}</span>
-            <span class="isMor">{{receiveAddress.isMor==0?'':'默认'}}</span>
+            <span class="isMor">{{receiveAddress.isMor == 0 ? '' : '默认'}}</span>
             <span class="type">收货地址</span>
             <i class="icon iconfont icon-more more"></i>
         </div>
         <div v-else>
-            <p @click="gotoReceiveAddress" class="noaddress-container pannel">还没有任何收货地址，立即添加 <i class="icon iconfont icon-more"></i></p>
+            <p @click="gotoReceiveAddress" class="noaddress-container pannel">还没有任何收货地址，立即添加 <i
+                class="icon iconfont icon-more"></i></p>
         </div>
         <!--发票地址-->
         <div v-if="invoiceAddress.addressid" class="address-container pannel">
@@ -171,13 +202,15 @@
             <span class="shouhr">{{invoiceAddress.shouhr}}</span>
             <span class="phone">{{invoiceAddress.phone}}</span>
             <span class="dizhi">{{invoiceAddress.dizhi}}</span>
-            <span class="isMor">{{invoiceAddress.isMor==0?'':'默认'}}</span>
+            <span class="isMor">{{invoiceAddress.isMor == 0 ? '' : '默认'}}</span>
             <span class="type">收票地址</span>
             <i class="icon iconfont icon-more more"></i>
         </div>
         <div v-else>
-            <p @click="gotoInvoiceAddress" class="noaddress-container pannel">还没有任何发票邮寄地址，立即添加 <i class="icon iconfont icon-more"></i></p>
+            <p @click="gotoInvoiceAddress" class="noaddress-container pannel">还没有任何发票邮寄地址，立即添加 <i
+                class="icon iconfont icon-more"></i></p>
         </div>
+        <div class="border"></div>
         <div class="cartList" v-for="cart in carts">
             <div class="goods-company">
                 <i class="icon iconfont icon-dianpu"></i>
@@ -206,50 +239,51 @@
             卖家留言
         </div>
         <div class="footer">
-            <span class="total-price">合计：<span class="fc-red">{{totalPrice}}</span></span><button class="confirm-btn">提交订单</button>
+            <span class="total-price">合计：<span class="fc-red">{{totalPrice}}</span></span>
+            <button class="confirm-btn">提交订单</button>
         </div>
     </div>
 </template>
 <script>
     export default {
-        name:'dingdan',
-        data(){
-            return{
-                totalPrice:'',
-                carts:[],
-                invoiceAddress:[],
-                receiveAddress:[]
+        name: 'dingdan',
+        data() {
+            return {
+                totalPrice: '',
+                carts: [],
+                invoiceAddress: [],
+                receiveAddress: []
             }
         },
-        mounted(){
+        mounted() {
             this.fetchData()
         },
-        computed:{
-            ids(){
+        computed: {
+            ids() {
                 return this.$route.query.ids
             }
         },
-        methods:{
-            fetchData(){
-                this.$http.post(this.$APIs.GO_BOOKING,{
-                    userid:localStorage.getItem('uid'),
-                    ids:this.ids
+        methods: {
+            fetchData() {
+                this.$http.post(this.$APIs.GO_BOOKING, {
+                    userid: localStorage.getItem('uid'),
+                    ids: this.ids
                 })
-                    .then(res=>{
+                    .then(res => {
                         console.log(res);
                         this.totalPrice = res.data.data.totalPrice;
                         this.carts = res.data.data.carts;
                         this.invoiceAddress = res.data.data.invoiceAddress;
                         this.receiveAddress = res.data.data.receiveAddress;
                     })
-                    .catch(err=>{
+                    .catch(err => {
                         this.$message.error('网络错误')
                     });
             },
-            gotoReceiveAddress(){
+            gotoReceiveAddress() {
                 this.$router.push('receiveAddress')
             },
-            gotoInvoiceAddress(){
+            gotoInvoiceAddress() {
                 this.$router.push('invoiceAddress')
             }
         }
